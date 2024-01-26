1c44c7f7dbbc1b262d3a92cc5e3f34219b131fee
SamRoy-07 <151897367+SamRoy-07@users.noreply.github.com>
2024-01-26 21:10:51 +0530

    Add files via upload

--- signup.php
+++ signup.php
@@ -2,7 +2,7 @@
 <?php
 $email=$_POST["email"];
 $password=$_POST["password"];
-$con=mysqli_connect('localhost','root','')or die('not connected');
+$con=mysqli_connect('localhost','root','') or die('not connected');
 mysqli_select_db($con,'project')or die('not selected');
 $sql="insert into sample values('$email','$password')";
 mysqli_query($con,$sql);
