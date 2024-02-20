58d4f57773838e7ba45e867e47a8d7337fc2b78a
SamRoy-07 <151897367+SamRoy-07@users.noreply.github.com>
2024-02-20 18:05:55 +0530

    Add files via upload

---
+++ main.php
@@ -0,0 +1,89 @@
+<html>
+    <head>
+        <style>
+            *{
+                margin:0%;
+                padding:0%;
+
+            }
+               .container2{
+                display:flex;
+                flex-direction:column;
+                background-color:whitesmoke;
+                height:85%;
+                width:20%;
+                position:absolute;
+                margin-top:5%;
+
+               }
+               body{
+                background-color:white;
+
+               }
+               .title{
+                display:flex;
+                flex-direction:column;
+                background-color:white;
+                position: absolute;
+                top: 2%;
+                left:4%;
+                font-weight:bold;
+               }
+
+               .leftnav{
+                display:flex;
+                flex-direction:column;
+                font-weight:bold;
+                text-decoration:none;
+                position:absolute;
+                margin-top:22%;
+                margin-left:8%;
+                         
+               }
+               .leftnav ul {
+                display: flex;
+                list-style-type: none;
+                gap: 20px;
+                flex-direction: column;
+                   
+            }
+               
+            </style>
+            </head>
+            <body>
+                <div class="container2">
+                    <div class="title">
+                        Pride
+                    </div>
+                    <div class="leftnav">
+            <ul>
+
+            <li>
+                    <a href="users.php">
+                        <p>Home</p>
+                    </a>
+                </li>
+                
+                <li>
+                    <a href="users.php">
+                        <p>News</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="Postimagef.php">
+                        <p>Contact</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="Postimagef.php">
+                        <p>About</p>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+
+                   
+            </div>
+</body>
+</html>
